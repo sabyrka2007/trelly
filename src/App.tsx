403ef72a541d6026a95f5f652f9925e150044901
Sabyrka <sabyrka2007@gmail.com>
2026-01-07 08:10:47 +0500

    feat: render algorithm

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
+const tasks = [
+  {
+    id: 1,
+    title: 'Купить продукты на неделю',
+    isDone: false,
+    addedAt: '1 сентября',
+  },
+  {
+    id: 2,
+    title: 'Полить цветы',
+    isDone: true,
+    addedAt: '2 сентября',
+  },
+  {
+    id: 3,
+    title: 'Сходить на тренировку',
+    isDone: false,
+    addedAt: '3 сентября',
+  },
+]
+
 export const App = () => {
-
-  const tasks = [
-    { id: 1, title: 'Купить продукты на неделю', isDone: false },
-    { id: 2, title: 'Полить цветы', isDone: true },
-    { id: 3, title: 'Сходить на тренировку', isDone: false },
-  ]
-
   return (
     <>
       <h1>Список дел</h1>
       <ul>
         {tasks.map((task) => (
           <li key={task.id}>
-            <div>{task.title}</div>
-            <input
-              type="checkbox"
-              checked={task.isDone}
-              readOnly
-            />
+            <div>
+              <b>Заголовок: </b>
+              <span>{task.title}</span>
+            </div>
+            <div>
+              <b>Статус: </b>
+              <input
+                type="checkbox"
+                checked={task.isDone}
+                readOnly
+              />
+            </div>
+            <div>
+              <b>Дата создания задачи: </b>
+              <span>{task.addedAt}</span>
+            </div>
           </li>
         ))}
       </ul>
